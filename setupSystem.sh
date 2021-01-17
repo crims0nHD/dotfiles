bb91147c841f99f069d8811bd7ee2f10c3e56d7b
David Hintringer <gamedavid.dh@gmail.com>
2021-01-17 21:51:49 +0100

    add setupSystem script

---
+++ setupSystem.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if running as root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Arch based distros
+if [ ! -z $(command -v pacman) ]
+then
+	pacman -Syu
+	pacman -S awesome rofi kitty fish neovim picom emacs fd ripgrep git
+
+# Debian based distros
+elif [ ! -z $(command -v apt) ]
+then
+	apt update
+	apt upgrade
+	apt install awesome rofi kitty fish neovim picom emacs fd-find ripgrep
+fi
+
