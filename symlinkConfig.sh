c5629c8da5cf06dc70f0b2025d64f0ed14c69d07
David Hintringer <gamedavid.dh@gmail.com>
2021-01-17 21:02:25 +0100

    add symlink script

---
+++ symlinkConfig.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -s $PWD/awesome ~/.config/awesome
+ln -s $PWD/fish ~/.config/fish
+ln -s $PWD/kitty ~/.config/kitty
+ln -s $PWD/nvim ~/.config/nvim
+ln -s $PWD/picom ~/.config/picom
+ln -s $PWD/rofi ~/.config/rofi
