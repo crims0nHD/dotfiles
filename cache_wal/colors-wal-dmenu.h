eadf455eceb43a331cd84eb027e6f71cd853bf69
David Hintringer <gamedavid.dh@gmail.com>
2021-03-17 20:14:36 +0100

    remove pywal schemes

--- cache_wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#d197e2", "#00001B" },
-	[SchemeSel] = { "#d197e2", "#64049F" },
-	[SchemeOut] = { "#d197e2", "#DC0FA5" },
-};
--- cache_wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#00001B";
-static const char* selfgcolor   = "#d197e2";
-static const char* normbgcolor  = "#7A4DBB";
-static const char* normfgcolor  = "#d197e2";
-static const char* urgbgcolor   = "#64049F";
-static const char* urgfgcolor   = "#d197e2";
