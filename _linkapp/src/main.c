40e68730d917593d8d9b47a1e1ab6076a1d5ee55
David Hintringer <gamedavid.dh@gmail.com>
2021-03-20 15:38:57 +0100

    add _linkapp

---
+++ _linkapp/src/main.c
@@ -0,0 +1,45 @@
+#include <curses.h>
+#include <menu.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const char *choices[] = {
+    "Hello this is tech support",
+    "Koniciwa",
+    "Exit",
+};
+
+size_t iarray_l;
+ITEM **iarray;
+MENU *menu;
+
+void startmenu() {
+  initscr();            // create window
+  raw();                // framebuffer magic
+  keypad(stdscr, TRUE); // enable input things
+  noecho();             // stop echoing input
+}
+
+void quitmenu() {
+  unpost_menu(menu); //?
+  free_menu(menu);
+
+  for (size_t i = 0; i < iarray_l; i++) {
+    free_item(iarray[i]);
+  }
+
+  free(iarray);
+  endwin(); // close ncurses window
+}
+
+int main(int argc, char **argv) {
+  startmenu();
+
+  refresh();
+
+  while (getch() != KEY_F(1))
+    ;
+
+  quitmenu(); // cleanup
+  return 0;
+}
