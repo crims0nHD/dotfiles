f21cc45274cb206649f23b2a1645448b34e97e2b
David Hintringer <gamedavid.dh@gmail.com>
2021-07-14 22:30:03 +0200

    Added install script; organized file structure

--- README.md
+++ README.md
@@ -7,13 +7,23 @@ Legal disclaimer
 Wallpaper: [Link To Wallpaper](https://www.pinterest.com/pin/458663543303636653/)
 I have no rights to this picture.
 
+Dependencies
+------------------
+TODO (Check output of install script)
+
+Installation
+------------------
+Run ./install.sh with either bash or zsh. Easy as that
+
+Removal
+------------------
+No script yet... Just remove the symlinks in home and home/.config (Tip: ls -la is your friend) 
 
 Status
 ------------------
 Todo:
-* Fix paths in scripts
-* Create a master script for controlling other scripts
 * Add dependency checking and automatic package installation
     * Gentoo
     * Arch
     * Debian
+* Add uninstall script
---
+++ install.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+# Check dependencies
+# ------------------------
+
+echo "Checking dependencies..."
+
+DEPS=(nvim curl emacs doas zsh pip cmake clang)
+for dep in "${DEPS[@]}"
+do
+    if [ -z $(which $dep) ]
+    then
+        echo "Error: $dep missing... Install $dep"
+        exit 1
+    else
+        echo "Found $dep"
+    fi
+done
+
+echo "Done checking dependencies"
+
+# Link config in .config
+# ------------------------
+
+echo "Creating .config if it doesn't exist already"
+mkdir -p $HOME/.config
+
+echo "Symlinking into .config"
+pushd ./config
+for f in ./*
+do
+    ln -s ${PWD}/$f ${HOME}/.config/$f
+    echo "Symlinked $f"
+done
+popd
+
+echo "Done symlinking .config"
+
+# Link config in home
+# ------------------------
+
+echo "Symlinking home"
+pushd ./home
+for f in ./*
+do
+    ln -s ${PWD}/$f ${HOME}/.$f
+    echo "Symlinked .$f"
+done
+popd
+
+echo "Done symlinking home"
+
+# Install stuff
+# ------------------------
+
+echo "Fetching and installing tools and utils"
+pushd ./scripts
+for f in ./get*.sh
+do
+    echo "Executing $f"
+    ./$f
+done
+popd
+echo "Done fetching tools and utils"
+
+echo "Installing plug for neovim"
+nvim +'PlugInstall --sync' +qa
+
+echo "Applying fixes and tweaks"
+pushd ./scripts
+for f in ./fix*.sh
+do
+    echo "Executing $f"
+    ./$f
+done
+popd
+echo "Done applying fixes"
+
+echo "Done with installation... Have a nice day!"
